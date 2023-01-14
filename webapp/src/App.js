d61ecce135f7e52c38d20b35b2914b233dc22221
Alina Zeng <alina.yzeng@gmail.com>
2023-01-14 14:40:17 -0500

    add homepage skeleton

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import Homepage from './Components/Pages/Homepage';
+import Homepage from './Components/Pages/Homepage';
 import { Route, Routes } from "react-router-dom";
 import SignIn from './Components/Pages/SignIn';
 import SignUp from './Components/Pages/SignUp';
 function App() {
   return (
     <div className="App">
-      Home
+        <Homepage />
     </div>
   );
 }
