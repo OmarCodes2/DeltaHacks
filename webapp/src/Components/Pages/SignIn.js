c3060ad4651275957a1b6063e801a6820760af49
OmarCodes2 <omarcodes2@gmail.com>
2023-01-14 14:28:56 -0500

    created sign up and sign in pages

---
+++ webapp/src/Components/Pages/SignIn.js
@@ -0,0 +1,15 @@
+
+function SignIn() {
+  return (
+    <div>
+        <div>Sign Up</div>
+        <form>
+            <input type="email" placeholder="example@example.com"/>
+            <input type="password"/>
+        </form>
+        <div>Have an account Sign in</div>
+    </div>
+  );
+}
+
+export default SignIn;
--- webapp/src/index.js
+++ webapp/src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import SignIn from './Components/Pages/SignIn';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+    <App/>
 );
 
 
