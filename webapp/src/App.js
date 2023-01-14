c14fc0a8c54bf5ebde444b9cc1f8381ea3253748
Alina Zeng <alina.yzeng@gmail.com>
2023-01-14 14:40:17 -0500

    add homepage skeleton

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Homepage from './Components/Pages/Homepage';
 
 function App() {
   return (
     <div className="App">
-      Home
+        <Homepage />
     </div>
   );
 }
