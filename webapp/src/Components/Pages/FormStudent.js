e33abdcfee87da8e20bbf6fec8305143affd52b8
Alina Zeng <alina.yzeng@gmail.com>
2023-01-15 00:05:38 -0500

    add minor changes

--- webapp/src/Components/Pages/FormStudent.js
+++ webapp/src/Components/Pages/FormStudent.js
@@ -4,9 +4,9 @@ import IntDropdown from "../IntDropdown";
 const FormStudent = () => {
     return(
         <section className="form-section">
-            <StringDropdown question="Select your program of study." items={["ibiomed", "engineering1", "music"]} responseType="program" />
+            <StringDropdown question="Select your program of study." items={["iBioMed", "Engineering 1", "Music"]} responseType="program" />
             <IntDropdown question="Select your year of study." items={["1", "2", "3", "4", "5"]} responseType="year"/>
-            <IntDropdown question="Select your gender identity." items={["male", "female", "other"]} responseType="gender"/>
+            <IntDropdown question="Select your gender identity." items={["Male", "Female", "Other"]} responseType="gender"/>
         </section>
     )
 }
